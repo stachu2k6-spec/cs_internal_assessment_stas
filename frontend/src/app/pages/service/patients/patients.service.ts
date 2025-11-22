650b2d8a90ffaa3c820f735575d4ee926aa7ab79
Stanislaw Jankowski <stachu2k6@gmail.com>
2025-11-22 15:07:33 +0100

    backend testing tutorial

--- frontend/src/app/pages/service/patients/patients.service.ts
+++ frontend/src/app/pages/service/patients/patients.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { PatientDto } from '@/pages/service/patients/patients.model';
 
 @Injectable({
@@ -13,7 +13,13 @@ export class PatientsService {
     constructor(private http: HttpClient) {}
 
     getAll(): Observable<PatientDto[]> {
-        return this.http.get<PatientDto[]>(this.apiUrl);
+        //return this.http.get<PatientDto[]>(this.apiUrl);
+
+        return of([
+            { id: 1, name: 'John', surname: 'Doe', address: '123 Main St', notes: 'Allergic to penicillin' },
+            { id: 2, name: 'Jane', surname: 'Smith', address: '456 Oak Ave', notes: 'Diabetic' },
+            { id: 3, name: 'Michael', surname: 'Johnson', address: '789 Pine Rd', notes: 'Asthmatic' }
+        ])
     }
 
     getById(id: number): Observable<PatientDto> {
